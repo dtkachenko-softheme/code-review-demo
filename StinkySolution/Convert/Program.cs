aceb3d53b03966fff5673105fb8d23c08f8452fa
Negligent Developer <negligent.developer@gmail.com>
2014-04-09 20:12:37 +0300

    Add converter application logic to convert days
    TASK ID: 7

---
+++ StinkySolution/Convert/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var days = int.Parse(args[0]);
+            var right = int.Parse(args[1]);
+
+            var result = (byte)(((days >> (right % 7)) | (days << (7 - (right % 7)))) & 127);
+
+            Console.WriteLine("The result of conversion is: {0}", result);
+        }
+    }
+}
