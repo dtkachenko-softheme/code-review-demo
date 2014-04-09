4f75ee8407bbd28402df3cd8a7661cf17bb9eb2b
Negligent Developer <negligent.developer@gmail.com>
2014-04-09 19:43:18 +0300

    Add user impersonator logic
    In addition to execution command as specified user advanced logging was added.
    Logs are stored per user in separate file.
    
    TASK ID: 5

---
+++ StinkySolution/UserImpersonator/UserImpersonatorProgram.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace UserImpersonator
+{
+    class UserImpersonatorProgram
+    {
+        private static UserLogger _impersonatedUserLogger;
+        private static UserImpersonator _impersonator;
+
+        static void Main(string[] args)
+        {
+            _impersonator = new UserImpersonator();
+
+            var stop = false;
+            while(!stop)
+            {
+                if (string.IsNullOrWhiteSpace(_impersonator.UserName))
+                {
+                    Console.Write("login:");
+                    _impersonator.UserName = Console.ReadLine();
+                    _impersonatedUserLogger = new UserLogger(_impersonator);
+                }
+
+                Console.Write(":");
+                var command = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(command))
+                {
+                    continue;
+                }
+
+                if (command.StartsWith("logout"))
+                {
+                    _impersonator.UserName = string.Empty;
+                }
+                else if (command.StartsWith("quit") || command.StartsWith("exit"))
+                {
+                    stop = true;
+                }
+                else
+                {
+                    _impersonator.ExecuteCommand(command);
+                }
+
+            }
+        }
+    }
+
+    
+
+    
+}
