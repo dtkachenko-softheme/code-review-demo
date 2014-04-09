0b20d8501f06b21f9c66f827619bbbd920d09a1a
Negligent Developer <negligent.developer@gmail.com>
2014-04-09 11:46:51 +0300

    Add echo command line application
    TASK ID: 2

---
+++ StinkySolution/Echo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stinky
+{
+    class EchoProgram
+    {
+        static void Main(string[] args)
+        {
+            var MY_LINE_CONSTRUCTOR = new StringBuilder();
+
+            foreach (var arg in args) MY_LINE_CONSTRUCTOR.AppendFormat("{0} ", arg);
+
+            Console.WriteLine(MY_LINE_CONSTRUCTOR.ToString());
+        }
+    }
+}
